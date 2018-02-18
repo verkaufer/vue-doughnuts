eefb6197454226b81fb9764ddb7851f6581bbecd
David Gunter <david.gunter@tivix.com>
2018-02-18 15:08:32 -0800

    Add basic lookup for all favorites a user previously recorded.

--- src/components/Favorites.vue
+++ src/components/Favorites.vue
@@ -1,14 +1,50 @@
 <template>
     <b-row>
       <b-col>
-        this is a list of favorites
+        <h3>Your Favorite Donut Shops</h3>
+        <div v-for="favorite in favorites" :key="favorite.id">
+          <p>{{favorite.data.name}} (placeID: {{favorite.id}})</p>
+        </div>
       </b-col>
     </b-row>
 </template>
 
 <script>
+  import { mapGetters } from 'vuex'
+
   export default {
-    name: 'favorites'
+    name: 'favorites',
+    created: function () {
+      this.loadFavorites()
+    },
+    data () {
+      return {
+        favorites: []
+      }
+    },
+    computed: {
+      ...mapGetters([
+        'user'
+      ])
+    },
+    methods: {
+      loadFavorites () {
+        this.$firestore
+          .collection('users')
+          .doc(this.user.uid)
+          .collection('favorites')
+          .where('created_on', '>', 0)
+          .get()
+          .then(favoritesSnapshot => {
+            favoritesSnapshot.forEach(favorite => {
+              this.favorites.push({
+                'id': favorite.id,
+                'data': favorite.data()
+              })
+            })
+          })
+      }
+    }
   }
 </script>
 
