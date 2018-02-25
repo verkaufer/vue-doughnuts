91482e8b44c9de5df35e29cb09cb732803fe3e60
David Gunter <david.gunter@tivix.com>
2018-02-25 15:14:30 -0800

    Add card UI component to Favorites page. Add ability to remove favorite shops from the Favorites page.

--- src/components/Favorites.vue
+++ src/components/Favorites.vue
@@ -2,24 +2,55 @@
     <b-row>
       <b-col>
         <h3>Your Favorite Donut Shops</h3>
-        <div v-for="favorite in favorites" :key="favorite.id">
-          <p>{{favorite.data.name}} (placeID: {{favorite.id}})</p>
+        <strong v-if="loading">Loading...</strong>
+
+        <div v-if="!loading">
+        <div v-if="!favorites.length">
+          <h4>Looks like you haven't saved any Donut Shops!</h4>
+          <p><router-link to="Home">Go find your favorites</router-link></p>
         </div>
+
+        <div v-if="favorites.length > 0">
+          <b-card-group columns class="mb-3">
+            <b-card v-for="(favorite, index) in favorites"
+                    border-variant="primary"
+                    v-bind:header="favorite.data.name"
+                    header-bg-variant="primary"
+                    header-text-variant="white"
+                    align="center">
+              <p class="card-text">
+                <span>{{favorite.data.approxLocation}}</span>
+                <br>
+                <b-button size="sm"
+                          :href="google_maps_link_root + favorite.id"
+                          rel="noopener noreferrer"
+                          target="_blank">Google Maps</b-button>
+                <b-button size="sm" @click="deleteFavorite(favorite.id)" variant="danger">Remove</b-button>
+              </p>
+            </b-card>
+          </b-card-group>
+        </div>
+        </div>
+        <!--<div v-for="favorite in favorites" :key="favorite.id">-->
+          <!--<p>{{favorite.data.name}} (placeID: {{favorite.id}})</p>-->
+        <!--</div>-->
       </b-col>
     </b-row>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex'
+  import { mapGetters, mapActions } from 'vuex'
 
   export default {
     name: 'favorites',
-    created: function () {
+    mounted: function () {
       this.loadFavorites()
     },
     data () {
       return {
-        favorites: []
+        favorites: [],
+        loading: true,
+        google_maps_link_root: 'https://www.google.com/maps/place/?q=place_id:'
       }
     },
     computed: {
@@ -28,6 +59,8 @@
       ])
     },
     methods: {
+      // Loading favorites should be moved into Vuex, but do we want
+      // to really store all this json info there??
       loadFavorites () {
         this.$firestore
           .collection('users')
@@ -36,6 +69,7 @@
           .where('created_on', '>', 0)
           .get()
           .then(favoritesSnapshot => {
+            this.loading = false
             favoritesSnapshot.forEach(favorite => {
               this.favorites.push({
                 'id': favorite.id,
@@ -43,7 +77,22 @@
               })
             })
           })
-      }
+          .catch(err => {
+            console.log(err) // appease linter for now
+            // Error loading
+            // FIXME: Add alert
+          })
+          .then(() => {
+            this.loading = false
+          })
+      },
+      deleteFavorite (shopId) {
+        this.removeFavoriteShop({userID: this.user.uid, shopId: shopId})
+        this.favorites = this.favorites.filter(favorite => favorite.id !== shopId)
+      },
+      ...mapActions([
+        'removeFavoriteShop'
+      ])
     }
   }
 </script>
